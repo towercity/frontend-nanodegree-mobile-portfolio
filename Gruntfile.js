0b0886b69e67d0ce031f2af7074655b277e1cab8
Matthew Nerger <mnerger@lsu.edu>
2016-03-27 14:26:20 -0500

    Set up grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,71 +1,86 @@
-/*
- After you have changed the settings at "Your code goes here",
- run this with one of these options:
-  "grunt" alone creates a new, completed images directory
-  "grunt clean" removes the images directory
-  "grunt responsive_images" re-processes images without removing the old ones
-*/
-
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    responsive_images: {
-      dev: {
-        options: {
-          engine: 'im',
-          sizes: [{
-            width: 293,
-            quality: 30,
-            suffix: '_small'
-          }]
+    grunt.initConfig({
+        uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: {
+                    'dist/js/perfmatters.min.js': ['src/js/perfmatters.js'],
+                    'dist/views/js/main.min.js': ['src/views/js/main.js']
+                }
+            }
         },
 
-        /*
-        You don't need to change this part if you don't change
-        the directory structure.
-        */
-        files: [{
-          expand: true,
-          src: ['pizzeria.{gif,jpg,png}'],
-          cwd: 'views/images/',
-          dest: 'views/images/'
-        }]
-      }
-    },
-
-    /* Clear out the images directory if it exists */
-    clean: {
-      dev: {
-        src: ['images'],
-      },
-    },
-
-    /* Generate the images directory if it is missing */
-    mkdir: {
-      dev: {
-        options: {
-          create: ['images']
+        cssmin: {
+            target: {
+                files: [{
+                    'dist/views/css/style.min.css': ['src/views/css/style.css', 'src/views/css/style.css'],
+                    'dist/css/print.min.css': ['src/css/print.css']
+                }]
+            }
         },
-      },
-    },
 
-    /* Copy the "fixed" images that don't go through processing into the images/directory */
-    copy: {
-      dev: {
-        files: [{
-          expand: true,
-          src: 'images_src/fixed/*.{gif,jpg,png}',
-          dest: 'images/'
-        }]
-      },
-    }
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhiteSpace: true,
+                    minifyJS: true,
+                    minifyCSS: true
+                },
+                files: {
+                    'dist/index.html': 'src/index.html',
+                    'dist/project-2048.html': 'src/project-2048.html',
+                    'dist/project-mobile.html': 'src/project-mobile.html',
+                    'dist/project-webpref.html': 'src/project-webpref.html'
+                    'dist/views/pizza.html': 'src/views/pizza.html'
+                }
+            }
+        },
 
-  });
+        copy: {
+            build: {
+                cwd: 'src',
+                src: ['**'],
+                dest: 'dist',
+                expand: true
+            }
+        },
 
-  grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-mkdir');
-  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+        clean: {
+            build: {
+                src: ['dist']
+            },
+            stylesheets: {
+                src: [ 'dist/**/*.css', '!build/application.css' ]
+            },
+            scripts: {
+                src: [ 'dist/**/*.js', '!build/application.js' ]
+            }
+        }
+    });
 
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.registerTask(
+        'scripts',
+        'Compiles the javascript',
+        ['uglify', 'clean:scripts']
+    );
+    grunt.registerTask(
+        'stylesheets',
+        'Compiles the stylesheets',
+        ['cssmin', 'clean:stylesheets']
+    );
+    grunt.registerTask(
+        'build',
+        'Compiles all of the assets and copies the files to the build directory.',
+        ['clean', 'scripts', 'stylesheets', 'htmlmin', 'copy']
+    );
 };
